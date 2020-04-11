3f6fac296fb927fe9becbf474cd0b1663b674865
Zain Khan <zain.khaan147@gmail.com>
2020-04-11 20:17:10 +0500

    gestures part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -23,6 +23,7 @@ const mapDispatchToProps = dispatch => ({
 
 function RenderDish(props) {
     const dish = props.dish;
+    handleViewRef = ref => this.view = ref;
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200)
             return true;
@@ -32,6 +33,11 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000).then(endState => {
+                console.log(endState.finished ? 'finished' : 'cancelled')
+            })
         },
         onPanResponderEnd: (e, gestureState) => {
             if (recognizeDrag(gestureState))
@@ -43,7 +49,7 @@ function RenderDish(props) {
                             text: 'Cancle',
                             onPress: () => console.log('Cancle Pressed'),
                             style: 'cancel'
-                        }, 
+                        },
                         {
                             text: 'Okay',
                             onPress: () => props.favorite ? console.log('already favorite') : props.onPress(),
@@ -57,7 +63,13 @@ function RenderDish(props) {
     });
     if (dish != null) {
         return (
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
+            <Animatable.View
+                animation="fadeInDown"
+                duration={2000}
+                delay={1000}
+                ref={this, handleViewRef}
+                {...panResponder.panHandlers}
+            >
                 <Card
                     featuredTitle={dish.name}
                     image={{ uri: baseUrl + dish.image }}>
